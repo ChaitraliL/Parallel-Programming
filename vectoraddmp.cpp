52dbfdd1bc9b84c425b7e900afb4fbec4cb503c3
ChaitraliL <34446236+ChaitraliL@users.noreply.github.com>
2018-09-25 09:05:28 +0530

    Update vectoraddmp.cpp

--- vectoraddmp.cpp
+++ vectoraddmp.cpp
@@ -1,32 +1,31 @@
 #include<iostream>
 #include<omp.h>
-#include<time.h>
 using namespace std;
 #define n 100000
 
 int main()
 {
-	double start_time = clock();
 	int a[n],b[n],c[n];
-/*	for(int i = 0; i < n; i++)
+	for(int i = 0; i < n; i++)
 	{
 		a[i] = 1;
 		b[i] = 2;	
-	}*/ 
-		#pragma omp parallel for
-		for(int i = 0; i < n; i++)
-		{
-			a[i] = 1;
-			b[i] = 2;
-			c[i] = a[i] + b[i];
-		}
+	} 
+	
+	double start_time = omp_get_wtime();
+	#pragma omp parallel for
+	for(int i = 0; i < n; i++)
+	{
+		c[i] = a[i] + b[i];
+	}
+	double end_time = omp_get_wtime();
 
 	for(int i = 0; i < n; i++)
 	{
 		cout<<c[i]<<" ";
 	}	
-	double end_time = clock();
-	double result_time = (end_time - start_time) / CLOCKS_PER_SEC;
+	double result_time = (end_time - start_time);
 	cout<<"\n"<<result_time;
 	return 0;
 }
+//0.000244117
